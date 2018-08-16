798f197ac0be0a7a90b2fd796d0717179842fa90
Jake Levi <jakelevi1996@gmail.com>
2018-08-16 13:57:36 +0100

    Added check in thread logic to see if there are still experiments left

--- run_threads.py
+++ run_threads.py
@@ -1,9 +1,11 @@
 import threading
 import numpy as np
 from time import sleep
+import numpy as np
 
-# Number of threads to keep active
+# Number of threads to keep active and experiments for them to run
 NUM_THREADS = 5
+NUM_EXPERIMENTS = 13
 
 # List to store each of the threads
 thread_list = []
@@ -21,13 +23,14 @@ def experiment_func(arg, thread_num):
     print("Thread {} has finished waiting for {} seconds".format(
         thread_num, arg))
 
-# Keep looping until no more threads are left alive
+# Keep looping until no more experiments left
 while len(experiment_list) > 0:
     # Check each of the threads
     for thread_num, thread in enumerate(thread_list):
-        # If one of the threads is not alive, assign it a new experiment
-        # popped from the list
-        if not thread.is_alive():
+        # If a thread is not alive and there are still experiments left,
+        # pop an experiment from the list and assign to a new thread,
+        # replacing the old thread in thread_list
+        if (not thread.is_alive()) and (len(experiment_list) > 0):
             print("Thread {} is not alive".format(thread_num))
             arg = experiment_list.pop()
             thread_list[thread_num] = threading.Thread(
